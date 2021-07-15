ac908963494cd8f4be3d2f9cabe12c60da498a2c
guiialmeiida <gui.almeidacrz@gmail.com>
2021-07-15 08:53:45 -0300

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/imageSlider';
 
 import {
     Container,
-    Header
+    Header,
+    CarImages
 } from './styles';
 
 export function CarDetails() {
@@ -13,6 +15,9 @@ export function CarDetails() {
                 <BackButton onPress={() => { }} />
             </Header>
 
+            <CarImages>
+                <ImageSlider imagesUrl={['https://img1.gratispng.com/20171220/kiq/audi-png-car-image-5a3b1f1eb47de9.9104985015138240307393.jpg']} />
+            </CarImages>
         </Container>
     );
 }
