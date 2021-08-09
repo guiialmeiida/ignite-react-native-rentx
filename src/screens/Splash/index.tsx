c72cabb1f1f175a3d44ef31ba9267ec6d989eec1
guiialmeiida <gui.almeidacrz@gmail.com>
2021-08-09 08:11:54 -0300

    Splash Screen

--- src/screens/Splash/index.tsx
+++ src/screens/Splash/index.tsx
@@ -1,13 +1,40 @@
 import React from 'react';
+import { Button, StyleSheet, Dimensions } from 'react-native';
+
+import Animated, { useSharedValue, useAnimatedStyle } from 'react-native-reanimated';
+
+const WIDTH = Dimensions.get('window').width;
 
 import {
     Container
 } from './styles';
 
 export function Splash() {
+    const animation = useSharedValue(0);
+
+    const animatedStyles = useAnimatedStyle(() => {
+        return {
+            transform: [{ translateX: animation.value }]
+        }
+    });
+
+    function handleAnimationPosition() {
+        animation.value = Math.random() * (WIDTH - 100);
+    }
+
     return (
         <Container>
+            <Animated.View style={[styles.box, animatedStyles]} />
 
+            <Button title='Mover' onPress={handleAnimationPosition} />
         </Container>
     );
 }
+
+const styles = StyleSheet.create({
+    box: {
+        width: 100,
+        height: 100,
+        backgroundColor: 'red'
+    }
+});
