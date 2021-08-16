fbb23dd82e8d8773e9568ecf637a572a719d9092
guiialmeiida <gui.almeidacrz@gmail.com>
2021-08-16 08:51:52 -0300

    Acessibilidade em formulário

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,5 +1,10 @@
 import React from 'react';
-import { StatusBar } from 'react-native';
+import {
+    StatusBar,
+    KeyboardAvoidingView,
+    TouchableWithoutFeedback,
+    Keyboard,
+} from 'react-native';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
@@ -19,56 +24,59 @@ import {
 export function SignIn() {
 
     return (
-        <Container>
-            <StatusBar
-                barStyle='dark-content'
-                backgroundColor='transparent'
-                translucent
-            />
-            <Header>
-                <Title>
-                    Estamos{'\n'}quase lá.
-                </Title>
+        <KeyboardAvoidingView behavior='position' enabled>
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+                <Container>
+                    <StatusBar
+                        barStyle='dark-content'
+                        backgroundColor='transparent'
+                        translucent
+                    />
+                    <Header>
+                        <Title>
+                            Estamos{'\n'}quase lá.
+                        </Title>
 
-                <SubTitle>
-                    Faça seu login para começar{'\n'}
-                    uma experiência incrível.
-                </SubTitle>
-            </Header>
+                        <SubTitle>
+                            Faça seu login para começar{'\n'}
+                            uma experiência incrível.
+                        </SubTitle>
+                    </Header>
 
-            <Form>
-                <Input
-                    iconName='mail'
-                    placeholder='E-mail'
-                    keyboardType='email-address'
-                    autoCorrect={false}
-                    autoCapitalize='none'
-                />
+                    <Form>
+                        <Input
+                            iconName='mail'
+                            placeholder='E-mail'
+                            keyboardType='email-address'
+                            autoCorrect={false}
+                            autoCapitalize='none'
+                        />
 
-                <PasswordInput
-                    iconName='lock'
-                    placeholder='Senha'
-                    secureTextEntry
-                />
-            </Form>
+                        <PasswordInput
+                            iconName='lock'
+                            placeholder='Senha'
+                        />
+                    </Form>
 
-            <Footer>
-                <Button
-                    title='Login'
-                    onPress={() => { }}
-                    enabled={false}
-                    loading={false}
-                />
+                    <Footer>
+                        <Button
+                            title='Login'
+                            onPress={() => { }}
+                            enabled={false}
+                            loading={false}
+                        />
 
-                <Button
-                    title='Criar conta gratuita'
-                    onPress={() => { }}
-                    color={theme.colors.background_secondary}
-                    enabled={true}
-                    loading={false}
-                    light
-                />
-            </Footer>
-        </Container>
+                        <Button
+                            title='Criar conta gratuita'
+                            onPress={() => { }}
+                            color={theme.colors.background_secondary}
+                            enabled={true}
+                            loading={false}
+                            light
+                        />
+                    </Footer>
+                </Container>
+            </TouchableWithoutFeedback>
+        </KeyboardAvoidingView>
     );
 }
